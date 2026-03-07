7a06445c787589ccd4d1fe7ee10f38fc25ae92e7
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-07 21:12:34 +0400

    refactor(storage): convert StorageModule to dynamic module with forRoot()

--- src/modules/storage/storage.module.ts
+++ src/modules/storage/storage.module.ts
@@ -1,35 +1,44 @@
 import { StorageController } from '@/api/storage/storage.controller';
 import { AppLogger } from '@/common/logger/app-logger.service';
 import { MinioClient } from '@/common/types/providers.type';
-import { Global, Module, Provider } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Client } from 'minio';
 import { StorageService } from './storage.service';
 
-const minioClientProvider: Provider = {
-  provide: MinioClient,
-  useFactory: (configService: ConfigService) => {
-    const host = configService.getOrThrow<string>('S3_HOST');
-    const port = configService.getOrThrow<number>('S3_PORT');
-    const accessKey = configService.getOrThrow<string>('S3_USERNAME');
-    const secretKey = configService.getOrThrow<string>('S3_PASSWORD');
-    const useSSL = configService.get<string>('S3_USE_SSL', 'false') === 'true';
-
-    return new Client({
-      endPoint: host,
-      port: parseInt(port.toString(), 10),
-      useSSL,
-      accessKey,
-      secretKey,
-    });
-  },
-  inject: [ConfigService],
-};
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Client } from 'minio';
 
 @Global()
-@Module({
-  providers: [minioClientProvider, StorageService, AppLogger],
-  exports: [StorageService],
-  controllers: [StorageController],
-})
-export class StorageModule {}
+@Module({})
+export class StorageModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: StorageModule,
+      providers: [
+        {
+          provide: MinioClient,
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => {
+            const host = configService.getOrThrow<string>('S3_HOST');
+            const port = configService.getOrThrow<number>('S3_PORT');
+            const accessKey = configService.getOrThrow<string>('S3_USERNAME');
+            const secretKey = configService.getOrThrow<string>('S3_PASSWORD');
+            const useSSL =
+              configService.get<string>('S3_USE_SSL', 'false') === 'true';
+
+            return new Client({
+              endPoint: host,
+              port: parseInt(port.toString(), 10),
+              useSSL,
+              accessKey,
+              secretKey,
+            });
+          },
+        },
+        StorageService,
+        AppLogger,
+      ],
+      exports: [StorageService],
+      controllers: [StorageController],
+    };
+  }
+}
