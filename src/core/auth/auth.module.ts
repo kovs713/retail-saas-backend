d3a70bea16c9739a63dd84b5064ddb60888b7c60
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-26 01:35:53 +0400

    chore: organize imports

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -1,8 +1,8 @@
+import { ShopModule } from '@/modules/shop/shop.module';
 import { UserModule } from '@/modules/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
-import { ShopModule } from '@/app/modules/shop/shop.module';
 import { Module } from '@nestjs/common';
 
 @Module({
