1c709914697a379828ffe2e404c5852bf59d0298
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-26 13:08:47 +0400

    fix(decorators): export factory functions, fix tests to test real logic

--- src/common/decorators/tenant.decorator.ts
+++ src/common/decorators/tenant.decorator.ts
@@ -2,9 +2,11 @@ import { Request, TenantContext } from '../types';
 
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const Tenant = createParamDecorator((data: unknown, ctx: ExecutionContext): TenantContext => {
+export const tenantFactory = (_data: unknown, ctx: ExecutionContext): TenantContext => {
   const request = ctx.switchToHttp().getRequest<Request>();
   return {
     shopId: request.user.shopId,
   };
-});
+};
+
+export const Tenant = createParamDecorator(tenantFactory);
