dabb17a7c343b3e9aa086b419f6dd700297b4667
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-26 23:41:35 +0400

    feat: add CORS rules

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
