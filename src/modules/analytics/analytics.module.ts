8526f03eed13487d0d82ec15e7e0d108141a9668
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-26 22:14:35 +0400

    feat(analytics): add stock report CSV export endpoint

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,3 +1,4 @@
+import { ProductModule } from '@/modules/product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
@@ -7,7 +8,7 @@ import { AnalyticsRepository } from './repositories';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatEvent, StorefrontView])],
+  imports: [TypeOrmModule.forFeature([ChatEvent, StorefrontView]), ProductModule],
   providers: [AnalyticsService, AnalyticsRepository],
   exports: [AnalyticsService],
   controllers: [AnalyticsController],
